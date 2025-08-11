23d4687f2de9b83e7fb641b058aa06c4ddb589a0
isaabv <isa148ly@gmail.com>
2025-08-10 23:16:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,156 @@
-const ramos = {
-  "Biología I": ["Biología II"],
-  "Comprensión y expresión lingüística": ["Ciencias sociales"],
-  "Desarrollo y destreza para el aprendizaje": [],
-  "Extra académica cultural": [],
-  "Extra académica deportiva": [],
-  "Laboratorio de biología I": ["Laboratorio de biología II"],
-  "Matemáticas I": ["Física médica", "Estadística general"],
-  "Química general": ["Química orgánica"],
-  "Biología II": ["Bioquímica", "Embriología", "Anatomía I"],
-  "Estadística general": ["Informática"],
-  "Física médica": [],
-  "Inglés instrumental agrobiologica": [],
-  "Laboratorio de biología II": ["Anatomía I", "Embriología"],
-  "Química orgánica": ["Bioquímica"],
-  "Sociología de la salud": ["Ciencias sociales", "Itpp I"],
-  "Anatomía I": ["Anatomía II", "Itpp II"],
-  "Ciencias sociales": ["Estadística"],
-  "Embriología": ["Anatomía II", "Genética", "Psicología evolutiva"],
-  "Itpp I": ["Itpp II"],
-  "Anatomía II": ["Fisiología", "Histología"],
-  "Bioquímica": ["Fisiología"],
-  "Informática": ["Estadística"],
-  "Itpp II": ["Itpp III"],
-  "Psicología evolutiva": ["Psicología médica"],
-  "Estadística": ["Epidemiología general y saneamiento ambiental"],
-  "Fisiología": ["Microbiología e inmunología clínica", "Psicología médica", "Fisiopatología", "Parasitología"],
-  "Histología": ["Microbiología e inmunología clínica", "Fisiopatología", "Parasitología"],
-  "Itpp III": ["Itpp IV"],
-  "Epidemiología general y saneamiento ambiental": ["Epidemiología especial"],
-  "Itpp IV": [],
-  "Microbiología e inmunología clínica": ["Anatomía patológica", "Semiología (medicina I)"],
-  "Parasitología": ["Anatomía patológica", "Semiología (medicina I)"],
-  "Psicología médica": ["Puericultura", "Psicopatología"],
-  "Anatomía patológica": ["Cirugía I", "Ginecología y obstetricia I"],
-  "Fisiopatología": ["Farmacología I", "Medicina II"],
-  "Semiología (medicina I)": ["Cirugía I", "Ginecología y obstetricia I", "Puericultura", "Medicina II", "Psicopatología", "Genética"],
-  "Cirugía I": ["Cirugía II"],
-  "Farmacología I": ["Farmacología II", "Pediatría I"],
-  "Genética": [],
-  "Ginecología y obstetricia I": ["Ginecología y obstetricia II"],
-  "Puericultura": ["Pediatría I"],
-  "Farmacología II": ["Medicina III", "Pediatría II"],
-  "Medicina II": ["Medicina III", "Imagenología"],
-  "Pediatría I": ["Pediatría II"],
-  "Psicopatología": ["Psiquiatría clínica"],
-  "Cirugía II": ["Medicina legal", "Cirugía III"],
-  "Deontología médica": ["Administración médica"],
-  "Epidemiología especial": ["Administración médica"],
-  "Ginecología y obstetricia II": ["Medicina legal", "Ginecología y obstetricia III"],
-  "Medicina III": ["Medicina IV"],
-  "Administración médica": [],
-  "Imagenología": ["Cirugía III", "Medicina del trabajo"],
-  "Medicina IV": ["Medicina V"],
-  "Medicina legal": ["Psiquiatría clínica"],
-  "Pediatría II": ["Pediatría III"],
-  "Cirugía III": ["Cirugía IV"],
-  "Historia de la medicina": [],
-  "Medicina del trabajo": ["Pasantía rural"],
-  "Medicina V": ["Medicina VI"],
-  "Psiquiatría clínica": ["Higiene mental y psicoterapia"],
-  "Cirugía IV": [],
-  "Ginecología y obstetricia III": [],
-  "Higiene mental y psicoterapia": [],
-  "Medicina VI": [],
-  "Pasantía rural": [],
-  "Pediatría III": [],
-  "Trabajo de grado": [],
-  "Sepa Diosito": []
+const materias = {
+  "Primer semestre": {
+    "Biología I": ["Biología II"],
+    "Comprensión y expresión lingüística": ["Ciencias sociales"],
+    "Desarrollo y destreza para el aprendizaje": [],
+    "Extra académica cultural": [],
+    "Extra académica deportiva": [],
+    "Laboratorio de biología I": ["Laboratorio de biología II"],
+    "Matemáticas I": ["Física médica", "Estadística general"],
+    "Química general": ["Química orgánica"]
+  },
+  "Segundo semestre": {
+    "Biología II": ["Bioquímica", "Embriología", "Anatomía I"],
+    "Estadística general": ["Informática"],
+    "Física médica": [],
+    "Inglés instrumental agrobiologica": [],
+    "Laboratorio de biología II": ["Anatomía I", "Embriología"],
+    "Química orgánica": ["Bioquímica"],
+    "Sociología de la salud": ["Ciencias sociales", "Itpp I"]
+  },
+  "Tercer semestre": {
+    "Anatomía I": ["Anatomía II", "Itpp II"],
+    "Ciencias sociales": ["Estadística"],
+    "Embriología": ["Anatomía II", "Genética", "Psicología evolutiva"],
+    "Itpp I": ["Itpp II"]
+  },
+  "Cuarto semestre": {
+    "Anatomía II": ["Fisiología", "Histología"],
+    "Bioquímica": ["Fisiología"],
+    "Informática": ["Estadística"],
+    "Itpp II": ["Itpp III"],
+    "Psicología evolutiva": ["Psicología médica"]
+  },
+  "Quinto semestre": {
+    "Estadística": ["Epidemiología general y saneamiento ambiental"],
+    "Fisiología": ["Microbiología e inmunología clínica", "Psicología médica", "Fisiopatología", "Parasitología"],
+    "Histología": ["Microbiología e inmunología clínica", "Fisiopatología", "Parasitología"],
+    "Itpp III": ["Itpp IV"]
+  },
+  "Sexto semestre": {
+    "Epidemiología general y saneamiento ambiental": ["Epidemiología especial"],
+    "Itpp IV": [],
+    "Microbiología e inmunología clínica": ["Anatomía patológica", "Semiología (medicina I)"],
+    "Parasitología": ["Anatomía patológica", "Semiología (medicina I)"],
+    "Psicología médica": ["Puericultura", "Psicopatología"]
+  },
+  "Séptimo semestre": {
+    "Anatomía patológica": ["Cirugía I", "Ginecología y obstetricia I"],
+    "Fisiopatología": ["Farmacología I", "Medicina II"],
+    "Semiología (medicina I)": ["Cirugía I", "Ginecología y obstetricia I", "Puericultura", "Medicina II", "Psicopatología", "Genética"]
+  },
+  "Octavo semestre": {
+    "Cirugía I": ["Cirugía II"],
+    "Farmacología I": ["Farmacología II", "Pediatría I"],
+    "Genética": [],
+    "Ginecología y obstetricia I": ["Ginecología y obstetricia II"],
+    "Puericultura": ["Pediatría I"]
+  },
+  "Noveno semestre": {
+    "Farmacología II": ["Medicina III", "Pediatría II"],
+    "Medicina II": ["Medicina III", "Imagenología"],
+    "Pediatría I": ["Pediatría II"],
+    "Psicopatología": ["Psiquiatría clínica"]
+  },
+  "Décimo semestre": {
+    "Cirugía II": ["Medicina legal", "Cirugía III"],
+    "Deontología médica": ["Administración médica"],
+    "Epidemiología especial": ["Administración médica"],
+    "Ginecología y obstetricia II": ["Medicina legal", "Ginecología y obstetricia III"],
+    "Medicina III": ["Medicina IV"]
+  },
+  "Décimo primer semestre": {
+    "Administración médica": [],
+    "Imagenología": ["Cirugía III", "Medicina del trabajo"],
+    "Medicina IV": ["Medicina V"],
+    "Medicina legal": ["Psiquiatría clínica"],
+    "Pediatría II": ["Pediatría III"]
+  },
+  "Décimo segundo semestre": {
+    "Cirugía III": ["Cirugía IV"],
+    "Historia de la medicina": [],
+    "Medicina del trabajo": ["Pasantía rural"],
+    "Medicina V": ["Medicina VI"],
+    "Psiquiatría clínica": ["Higiene mental y psicoterapia"]
+  },
+    "Décimo tercer semestre": {
+    "Cirugía IV": [],
+    "Ginecología y obstetricia III": [],
+    "Higiene mental y psicoterapia": [],
+    "Medicina VI": [],
+    "Pasantía rural": [],
+    "Pediatría III": [],
+    "Trabajo de grado": []
+  },
+  "Décimo cuarto semestre": {
+    "Sepa Diosito": []
 };
 
-const estado = {};
-const contenedor = document.getElementById("malla");
+const estadoMaterias = {};
 
-Object.keys(ramos).forEach(nombre => {
-  estado[nombre] = false;
-  const div = document.createElement("div");
-  div.className = "ramo bloqueado";
-  div.textContent = nombre;
-  div.onclick = () => aprobarRamo(nombre);
-  div.id = nombre.replace(/\s+/g, "_");
-  contenedor.appendChild(div);
-});
+function crearMalla() {
+  const contenedor = document.getElementById("malla");
+
+  for (const [semestre, ramos] of Object.entries(materias)) {
+    const divSemestre = document.createElement("div");
+    divSemestre.className = "semestre";
+    divSemestre.innerHTML = `<h2>${semestre}</h2>`;
+
+    for (const ramo in ramos) {
+      const divRamo = document.createElement("div");
+      divRamo.className = "ramo bloqueado";
+      divRamo.textContent = ramo;
+      divRamo.dataset.nombre = ramo;
+      divRamo.onclick = () => aprobarRamo(ramo);
+      divSemestre.appendChild(divRamo);
+      estadoMaterias[ramo] = { aprobado: false, requisitos: [], desbloquea: ramos[ramo] };
+    }
+
+    contenedor.appendChild(divSemestre);
+  }
+
+  desbloquearIniciales();
+}
 
 function desbloquearIniciales() {
-  const iniciales = [
-    "Biología I", "Comprensión y expresión lingüística", "Desarrollo y destreza para el aprendizaje",
-    "Extra académica cultural", "Extra académica deportiva", "Laboratorio de biología I",
-    "Matemáticas I", "Química general"
-  ];
-  iniciales.forEach(nombre => {
-    document.getElementById(nombre.replace(/\s+/g, "_")).classList.remove("bloqueado");
-  });
+  for (const ramo in estadoMaterias) {
+    const requisitos = Object.values(materias).flatMap(obj =>
+      Object.entries(obj).filter(([nombre, desbloquea]) => desbloquea.includes(ramo)).map(([nombre]) => nombre)
+    );
+    estadoMaterias[ramo].requisitos = requisitos;
+
+    if (requisitos.length === 0) {
+      document.querySelector(`[data-nombre="${ramo}"]`).classList.remove("bloqueado");
+    }
+  }
 }
 
 function aprobarRamo(nombre) {
-  if (estado[nombre]) return;
-  estado[nombre] = true;
-  const div = document.getElementById(nombre.replace(/\s+/g, "_"));
+  const ramo = estadoMaterias[nombre];
+  const div = document.querySelector(`[data-nombre="${nombre}"]`);
+
+  if (div.classList.contains("bloqueado") || ramo.aprobado) return;
+
+  ramo.aprobado = true;
   div.classList.add("aprobado");
-  const desbloqueos = ramos[nombre];
-  desbloqueos.forEach(r => {
-    const ramoDiv = document.getElementById(r.replace(/\s+/g, "_"));
-    ramoDiv.classList.remove("bloqueado");
-  });
+
+  for (const desbloqueado of ramo.desbloquea) {
+    const requisitos = estadoMaterias[desbloqueado].requisitos;
+    const todosAprobados = requisitos.every(req => estadoMaterias[req]?.aprobado);
+    if (todosAprobados) {
+      document.querySelector(`[data-nombre="${desbloqueado}"]`).classList.remove("bloqueado");
+    }
+  }
 }
 
-desbloquearIniciales();
+crearMalla();
